f1e03baf3e747da18e0cac528348812997477815
adriano-leal <adriano.ramos@acad.pucrs.br>
2020-03-19 19:15:04 -0300

    movie details view controlles files added

---
+++ movieDB_objectiveC/Controllers/Details/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  movieDB_objectiveC
+//
+//  Created by Adriano Ramos on 19/03/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ movieDB_objectiveC/Controllers/Details/DetailsViewController.m
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.m
+//  movieDB_objectiveC
+//
+//  Created by Adriano Ramos on 19/03/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
