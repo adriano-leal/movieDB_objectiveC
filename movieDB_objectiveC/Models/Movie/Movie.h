aee415d5c5b2de52e09a40156ce42215dd1403d4
adriano-leal <adriano.ramos@acad.pucrs.br>
2020-03-19 19:10:34 -0300

    groups added | models created | initial setup done

--- movieDB_objectiveC/Models/Movie/Movie.h
+++ movieDB_objectiveC/Models/Movie/Movie.h
@@ -1,15 +1,14 @@
 //
-//  ViewController.h
+//  Movie.h
 //  movieDB_objectiveC
 //
-//  Created by Adriano Ramos on 17/03/20.
+//  Created by Adriano Ramos on 19/03/20.
 //  Copyright © 2020 Adriano Ramos. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface Movie : NSObject
 
 @end
-
---
+++ movieDB_objectiveC/Models/Movie/Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  movieDB_objectiveC
+//
+//  Created by Adriano Ramos on 19/03/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+@end
